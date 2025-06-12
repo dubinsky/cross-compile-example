a3d9dc4d78a25a3edab16a671976f2a79275320f
Leonid Dubinsky <dub@podval.org>
2025-06-11 22:06:12 -0400

    - cleanup;

--- more/src/main/scala/example/MoreMain.scala
+++ more/src/main/scala/example/MoreMain.scala
@@ -1,6 +1,8 @@
 package example
 
-object MoreMain:
-  def main(args: Array[String]): Unit =
+object MoreMain {
+  def main(args: Array[String]): Unit = {
     println("calling Main from MoreMain")
     Main.main(args)
+  }
+}
