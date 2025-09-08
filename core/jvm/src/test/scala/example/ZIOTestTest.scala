22eff995584c24d3b47273732d5eabe704dcd6d7
Leonid Dubinsky <dub@podval.org>
2025-09-08 00:51:50 -0400

    - updates;

--- core/jvm/src/test/scala/example/ZIOTestTest.scala
+++ core/jvm/src/test/scala/example/ZIOTestTest.scala
@@ -5,6 +5,7 @@ import zio.test._
 object ZIOTestTest extends ZIOSpecDefault {
   override def spec: Spec[TestEnvironment, Any] = suite("ZIOTestTest")(
     test("sq1") { assertTrue(SharedLibrary.instance.sq(1) == 1) },
-    test("sq2") { assertTrue(SharedLibrary.instance.sq(2) == 4) }
+    test("sq2") { assertTrue(SharedLibrary.instance.sq(2) == 4) },
+    test("double") { assertTrue(SharedInMixedProjectRoot.double(3) == 6) }
   )
 }
